016921e1918c7fa2e46f5bb6d651dd7423d83444
Andrey <pikachu_andrey@vk.com>
2026-07-31 21:15:33 +0900

    KolibriOS: Add missing fs implementation

--- library/std/src/sys/fs/kolibri/mod.rs
+++ library/std/src/sys/fs/kolibri/mod.rs
@@ -602,6 +602,12 @@ pub fn set_times_nofollow(_p: &Path, _times: FileTimes) -> io::Result<()> {
     unsupported()
 }
 
+pub fn set_perm_nofollow(_p: &Path, _perm: FilePermissions) -> io::Result<()> {
+    writeln!(crate::sys::pal::api::debugboard(), "unimplemented: ::set_perm_nofollow").unwrap();
+
+    unsupported()
+}
+
 pub fn rmdir(_p: &Path) -> io::Result<()> {
     writeln!(crate::sys::pal::api::debugboard(), "unimplemented: ::rmdir").unwrap();
 
